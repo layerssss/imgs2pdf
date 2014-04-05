8bf74414075ac49bf3694ec25d6a1a3c4859d88a
Michael Yin <layerssss@gmail.com>
2014-04-05 18:51:56 +0800

    initial_commit

---
+++ MainForm.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace imgs2pdf
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        public string[] Imgs;
+        public int DPI;
+        public string Filename;
+        private Process process;
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            var args = "-density " + this.DPI + " ";
+            foreach (var img in this.Imgs)
+            {
+                args += img + " ";
+            }
+            this.saveFileDialog1.FileName = this.Filename;
+            DialogResult result;
+            if ((result =this.saveFileDialog1.ShowDialog()) == DialogResult.OK)
+            {
+                this.Text = "Converting " + this.Imgs.Length + " images to " + this.saveFileDialog1.FileName + " ...";
+                args += "\"" + this.saveFileDialog1.FileName + "\""; 
+                this.Activate();
+                this.process = new Process();
+                this.process.StartInfo = new ProcessStartInfo(Application.ExecutablePath.Substring(0, Application.ExecutablePath.LastIndexOf('\\')) + "\\convert.exe", args);
+                this.process.StartInfo.UseShellExecute = false;
+                this.process.StartInfo.CreateNoWindow = true;
+                this.process.StartInfo.RedirectStandardError = true;
+                this.process.EnableRaisingEvents = true;
+                this.process.Exited += new EventHandler(this.process_Exited);
+                this.process.Start();
+            }
+            else {
+                this.DialogResult = result;
+                this.Close();
+            }
+        }
+
+        void process_Exited(object sender, EventArgs e)
+        {
+            if (this.process.ExitCode == 0)
+            {
+                MessageBox.Show("PDF Saved to " + this.saveFileDialog1.FileName, "imgs2pdf", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.DialogResult = DialogResult.OK;
+            }
+            else
+            {
+                MessageBox.Show(this.process.StandardError.ReadToEnd(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.DialogResult = DialogResult.Abort;
+            }
+            this.Close();
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                this.process.Kill();
+            }
+            catch { }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace imgs2pdf
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            if (args.Length == 0)
+            {
+                var alertKey = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Classes", true).CreateSubKey("imgs2pdf");
+                alertKey.SetValue("", "URL:Pdf2imgs Protocol");
+                alertKey.SetValue("URL Protocol", "");
+                var shellKey = alertKey.CreateSubKey("shell");
+                var openKey = shellKey.CreateSubKey("open");
+                var commandKey = openKey.CreateSubKey("command");
+                commandKey.SetValue("", "\"" + Application.ExecutablePath + "\" \"%1\"");
+                MessageBox.Show("imgs2pdf installed.", "imgs2pdf", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                var frags = args[0].Remove(0, "imgs2pdf:".Length).Split('|');
+                var mainForm = new MainForm()
+                {
+                    Filename = frags[0],
+                    DPI = Convert.ToInt32(frags[1]),
+                    Imgs = frags.Skip(2).ToArray()
+                };
+                Application.Run(mainForm);
+            }
+        }
+    }
+}
